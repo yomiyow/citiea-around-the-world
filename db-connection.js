a055c7e88ca86b32ebc0293388c751bd7a69177a
romeo <romeoquinones4@gmail.com>
2024-10-14 00:47:28 +0800

    Initial commit

---
+++ db-connection.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+// Create connection
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '2022100960Romeo',
+  database: 'world'
+});
+
+// Query
+const sql = 'SELECT * FROM city';
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    const jsonResult = JSON.stringify(result, null, 2);
+    fs.writeFile('cities.json', jsonResult, (err) => {
+      if (err) throw err;
+      console.log('Result saved to cities.json');
+    }); // getting data from database ...
+  });
+
+  console.log('Done!');
+});
